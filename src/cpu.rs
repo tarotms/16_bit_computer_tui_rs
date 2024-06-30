07bf8542808db88bb5a41c61471d3fc235029330
Morishita Michiru <tarotms@outlook.com>
2024-06-30 12:15:16 +0800

    add screen buffer

--- src/cpu.rs
+++ src/cpu.rs
@@ -26,36 +26,58 @@ impl CPU {
             ram: RAM::new(),
             register: [0; 8],
             frequency: 1,
-            cool_down: 200,
+            cool_down: 100,
         }
     }
 
     pub fn startup(&mut self) {
-        let cd = std::time::Duration::from_micros(self.cool_down);
+        let buffer_size = 512;
+        let mut buffer_screen = String::with_capacity(buffer_size);
+        let mut buffer_behind = String::with_capacity(buffer_size);
+    
+
+
+        let cd = std::time::Duration::from_millis(self.cool_down);
 
         loop {
+
             let start_time = std::time::Instant::now();
-            let cycles = 512;
+            let cycles = 16;
 
             /* kernel clock */
             for _ in 0..cycles {
+
                 self.pc.increment();
             
-                print!("\x1B[2J\x1B[H");
-                utils::msg("Program count", format!("0b{:016b}", self.pc.print()));
+                let pc_count = self.pc.print();
+
+                buffer_behind.push_str(&utils::format(
+                    "\x1B[2J\x1B[HProgram count Dec", format!("0b{:05}", pc_count))
+                );
+
+                buffer_behind.push_str(&utils::format(
+                    "Program count Bin", format!("0b{:016b})", pc_count))
+                );
 
                 for i in 0..8 {
-
-                    utils::msg(&format!("R{:01X}", i),
-                        format!("{:8b}", self.register[i]));
+                    buffer_behind.push_str(&utils::format(
+                    &format!("R{:01X}", i),format!("{:8b}", self.register[i])));
 
                 }
-                
-                self.show_frequency();
 
-                std::thread::sleep(cd);
+                buffer_behind.push_str(&utils::format(
+                    "Frequency", format!("{} Hz",  self.frequency())))
+
             }
 
+            std::mem::swap(&mut buffer_screen, &mut buffer_behind);
+            
+            print!("{}", buffer_screen);
+
+            buffer_behind.clear();
+
+            std::thread::sleep(cd);
+        
             let average_cycle_time  = start_time.elapsed().as_micros() as f64 / cycles as f64;
             self.frequency = (1_000_000.0 / average_cycle_time) as u64;
             
@@ -80,8 +102,8 @@ impl CPU {
         }
     }
 
-    pub fn show_frequency(&self) {
-        utils::msg("Calculated Frequency", format!("{} Hz", self.frequency));
+    pub fn frequency(&self) -> u64 {
+        self.frequency
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,17 @@ mod cpu;
 fn main() {
 
     utils::welcome();
-    println!("> Press s to begin");
+    utils::msg("System", "Press s to begin");
 
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).expect("Failed to read line");
 
     if input.trim().eq_ignore_ascii_case("s") {
-    let mut cpu = cpu::CPU::new();
-    cpu.startup();
-
+        let mut cpu = cpu::CPU::new();
+        cpu.startup();
     }
+
+    utils::msg("System", "exit");
 
 }
 
